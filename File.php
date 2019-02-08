a9ee0532260ab63d1504aa70d6675137a7700b17
erirad <erika.radziulyte@gmail.com>
2019-02-08 10:17:38 +0200

    be namespaces

---
+++ File.php
@@ -0,0 +1,21 @@
+<?php
+class File {
+
+    private $file;
+    protected $arr = array();
+    protected $input;
+
+    function __construct($file) {
+        $this->file = new SplFileObject($file);
+        $this->getValuesFromUploadFile();
+    }
+    private function getValuesFromUploadFile()
+    {
+        while (!$this->file->eof()) {
+            $line = trim($this->file->fgets());
+            array_push($this->arr, $line);
+        }
+        echo "Irasykite zodi: ";
+        $this->input = trim(fgets(STDIN, 1024));
+    }
+}
--- index.php
+++ index.php
@@ -1,39 +1,9 @@
 <?php
-$file = new SplFileObject("text.txt");
-$arr = array();
-while (!$file->eof()) {
-    $line = trim($file->fgets());
-    array_push($arr, $line);
-}
 
-include 'getValues.php';
-include 'syllables.php';
+include 'Matches.php';
+include 'Syllables.php';
 
-
-
-function printFinalResult($rezultatas)
-{
-// gauname rezultata, pasaliname nebereikalingus '0', atspausdiname kaip string
-    $rezultatas = str_replace ( "0","", $rezultatas );
-
-//pakeicia nelyginius - "-", lyginius- ""
-    foreach ($rezultatas as $key => $rez) {
-        if(is_numeric($rez) && $rez % 2 != 0) {
-            $rezultatas[$key] = "-";
-        }
-        if(is_numeric($rez) && $rez % 2 == 0) {
-            $rezultatas[$key] = "";
-        }
-        if($rezultatas[0] == "-") {
-            $rezultatas[$key] = "";
-        }
-    }
-
-    $rezultatas = implode("", $rezultatas);
-
-    return $rezultatas;
-
-}
-
-$rezultatas = printFinalResult($rezultatas);
-echo $rezultatas . "\n";
+$matches = new Matches();
+$finalArr = $matches->getfinalArr();
+$syllables = new Syllables($finalArr);
+echo $syllables->getResult();
